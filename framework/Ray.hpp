4080e8bbf38dd767883f56121c079a3c61cdcd22
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-09-04 18:06:10 +0200

    Die Judith darf jetzt lustig weiter proggen, add tranformRay methode

--- framework/Ray.hpp
+++ framework/Ray.hpp
@@ -1,7 +1,8 @@
 #ifndef RAY_HPP
 #define RAY_HPP
-
+#include <glm/glm.hpp>
 #include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 struct Ray
 {
@@ -11,12 +12,23 @@ struct Ray
     Ray():
     origin(0.0f, 0.0f, 0.0f),
     direction(1.0f, 0.0f, 0.0f)
-    {}
+    {};
 
     Ray (glm::vec3 const& origin_, glm::vec3 const& direction_):
     origin(origin_),
     direction(direction_)
-    {}
+    {};
+
+    Ray transformRay(glm::mat4 const& mat , Ray const& ray)
+    {
+        glm::vec4 ori{ray.origin , 1.0f}; //niemals or nennen :))
+        glm::vec4 diri{ray.direction , 0.0f};
+        
+        glm::vec3 trans_Origin(mat * ori);
+        glm::vec3 trans_Direction(mat * diri);
+
+        return Ray {trans_Origin , trans_Direction};
+    };
 };
 
 #endif //define RAY_HPP
