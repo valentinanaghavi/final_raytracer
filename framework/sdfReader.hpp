7d675824d6cfe0ff53c034de3853953849197183
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-09-07 11:27:07 +0200

    sdf without search material method - jetzt aber wirklich

--- framework/sdfReader.hpp
+++ framework/sdfReader.hpp
@@ -31,8 +31,7 @@
 
 struct SdfReader
 {
-    Material search_material_map(std::string const& search_name , Scene& scene);   
-
+    //std::shared_ptr<Material> search_material_map(std::string const& search_name , std::map <std::string name , Material> material_container);
     std::shared_ptr<Shape> search_shape_vector(std::string const& search_name , std::vector <std::shared_ptr<Shape>> shape_container);
 
     glm::mat4 translation(glm::vec3 trans_vec) ;
@@ -40,7 +39,6 @@ struct SdfReader
     glm::mat4 scale(glm::vec3 scale_vec) ;
     glm::mat4 rotation(glm::vec3 rotation_vec, float winkel) ;
 
-    //Scene scene;
     
 };
 
